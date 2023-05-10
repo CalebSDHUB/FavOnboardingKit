a22f5b9cd6890de0fd6f9e4cba6984fed2fa6bb5
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-05-10 18:22:30 +0200

    update model

---
+++ Sources/FavOnboardingKit/Models/Slide.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Caleb Danielsen on 09.05.2023.
+//
+
+import UIKit
+
+public struct Slide {
+    let image: UIImage
+    let title: String
+    
+    public init(image: UIImage, title: String) {
+        self.image = image
+        self.title = title
+    }
+}
